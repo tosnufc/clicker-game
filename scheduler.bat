cac64c4af79f9e5009647609900383bfde8fe1bf
tosnufc <tosnufc@gmail.com>
2026-03-19 02:12:03 +0700

    remove process restarting

--- scheduler.bat
+++ scheduler.bat
@@ -35,17 +35,17 @@ if "%now%"=="%last_run%" goto wait
 if "%now%"=="10:00" (
     call :log "Launching claim_8-hrs_workflow..."
     start "" claim_8-hrs_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%now%"=="18:00" (
     call :log "Launching claim_8-hrs_workflow..."
     start "" claim_8-hrs_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%now%"=="02:00" (
     call :log "Launching claim_8-hrs_workflow..."
     start "" claim_8-hrs_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 :: ##################################################
 
@@ -53,7 +53,7 @@ if "%now%"=="02:00" (
 if "%now%"=="10:10" (
     call :log "Launching claim_daily_workflow..."
     start "" claim_daily_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 :: ################################################
 
@@ -62,42 +62,42 @@ REM Mon=1, Tue=2, Wed=3, Thu=4, Fri=5, Sat=6, Sun=0
 if "%dow%"=="1" if "%now%"=="02:15" (
     call :log "Launching prime_recruitment_30_workflow..."
     start "" prime_recruitment_30_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="1" if "%now%"=="22:15" (
     call :log "Launching prime_recruitment_30_workflow..."
     start "" prime_recruitment_30_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="2" if "%now%"=="14:15" (
     call :log "Launching prime_recruitment_30_workflow..."
     start "" prime_recruitment_30_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="3" if "%now%"=="10:20" (
     call :log "Launching prime_recruitment_30_workflow..."
     start "" prime_recruitment_30_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="4" if "%now%"=="18:15" (
     call :log "Launching prime_recruitment_30_workflow..."
     start "" prime_recruitment_30_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="5" if "%now%"=="06:15" (
     call :log "Launching prime_recruitment_30_workflow..."
     start "" prime_recruitment_30_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="6" if "%now%"=="02:15" (
     call :log "Launching prime_recruitment_30_workflow..."
     start "" prime_recruitment_30_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="6" if "%now%"=="16:15" (
     call :log "Launching prime_recruitment_30_workflow..."
     start "" prime_recruitment_30_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 :: ###############################################################
 
@@ -106,42 +106,42 @@ REM Mon=1, Tue=2, Wed=3, Thu=4, Fri=5, Sat=6, Sun=0
 if "%dow%"=="1" if "%now%"=="20:00" (
     call :log "Launching boomer_workflow..."
     start "" boomer_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="2" if "%now%"=="05:00" (
     call :log "Launching boomer_workflow..."
     start "" boomer_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="3" if "%now%"=="00:15" (
     call :log "Launching boomer_workflow..."
     start "" boomer_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="4" if "%now%"=="00:30" (
     call :log "Launching boomer_workflow..."
     start "" boomer_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="4" if "%now%"=="14:00" (
     call :log "Launching boomer_workflow..."
     start "" boomer_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="5" if "%now%"=="10:20" (
     call :log "Launching boomer_workflow..."
     start "" boomer_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="6" if "%now%"=="14:00" (
     call :log "Launching boomer_workflow..."
     start "" boomer_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="0" if "%now%"=="10:20" (
     call :log "Launching boomer_workflow..."
     start "" boomer_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 :: #################################################
 
@@ -149,12 +149,12 @@ if "%dow%"=="0" if "%now%"=="10:20" (
 if "%now%"=="11:59" (
     call :log "Launching auto_join_workflow..."
     start "" auto_join_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%now%"=="23:59" (
     call :log "Launching auto_join_workflow..."
     start "" auto_join_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 :: ##############################################
 
@@ -163,42 +163,42 @@ REM Mon=1, Tue=2, Wed=3, Thu=4, Fri=5, Sat=6, Sun=0
 if "%dow%"=="2" if "%now%"=="03:30" (
     call :log "Launching soldiers_popping_workflow..."
     start "" soldiers_popping_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="2" if "%now%"=="23:30" (
     call :log "Launching soldiers_popping_workflow..."
     start "" soldiers_popping_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="3" if "%now%"=="15:30" (
     call :log "Launching soldiers_popping_workflow..."
     start "" soldiers_popping_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="4" if "%now%"=="10:55" (
     call :log "Launching soldiers_popping_workflow..."
     start "" soldiers_popping_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="5" if "%now%"=="20:30" (
     call :log "Launching soldiers_popping_workflow..."
     start "" soldiers_popping_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="6" if "%now%"=="07:30" (
     call :log "Launching soldiers_popping_workflow..."
     start "" soldiers_popping_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="0" if "%now%"=="03:30" (
     call :log "Launching soldiers_popping_workflow..."
     start "" soldiers_popping_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="0" if "%now%"=="20:30" (
     call :log "Launching soldiers_popping_workflow..."
     start "" soldiers_popping_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 :: ###########################################################
 
@@ -207,42 +207,42 @@ REM Mon=1, Tue=2, Wed=3, Thu=4, Fri=5, Sat=6, Sun=0
 if "%dow%"=="1" if "%now%"=="14:30" (
     call :log "Launching soldiers_training_workflow..."
     start "" soldiers_training_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="2" if "%now%"=="10:30" (
     call :log "Launching soldiers_training_workflow..."
     start "" soldiers_training_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="3" if "%now%"=="02:30" (
     call :log "Launching soldiers_training_workflow..."
     start "" soldiers_training_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="3" if "%now%"=="22:55" (
     call :log "Launching soldiers_training_workflow..."
     start "" soldiers_training_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="5" if "%now%"=="07:30" (
     call :log "Launching soldiers_training_workflow..."
     start "" soldiers_training_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="5" if "%now%"=="20:45" (
     call :log "Launching soldiers_training_workflow..."
     start "" soldiers_training_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="6" if "%now%"=="14:55" (
     call :log "Launching soldiers_training_workflow..."
     start "" soldiers_training_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 if "%dow%"=="0" if "%now%"=="07:30" (
     call :log "Launching soldiers_training_workflow..."
     start "" soldiers_training_workflow.bat
-    goto restart
+    set "last_run=%now%"
 )
 :: ######################################################################
 
@@ -250,12 +250,6 @@ if "%dow%"=="0" if "%now%"=="07:30" (
 :: Wait 30 seconds before checking again
 timeout /t 30 /nobreak >nul
 goto loop
-
-:restart
-call :log "Restarting scheduler..."
-timeout /t 5 /nobreak >nul
-start "" "%~f0"
-exit
 
 :log
 echo [%date% %time%] %~1

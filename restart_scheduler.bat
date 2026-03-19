8a9d4c4b960ef46b8d8bbe6db3ef50fc6d6fca00
tosnufc <tosnufc@gmail.com>
2026-03-19 14:36:27 +0700

    also kill game dialog boxes

--- restart_scheduler.bat
+++ restart_scheduler.bat
@@ -1,4 +1,8 @@
 @echo off
+echo === Restarting scheduler on local PC ===
+.venv\Scripts\python.exe scheduler_restart.py
+echo.
+
 for /L %%i in (60,1,64) do (
     echo === Restart scheduler on 192.168.1.%%i ===
     "C:\Program Files\Git\usr\bin\ssh.exe" user@192.168.1.%%i "d: && cd dev\clicker-game && .venv\Scripts\python.exe scheduler_restart.py && exit"

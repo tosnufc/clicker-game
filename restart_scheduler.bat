358b6a26f5cdf987101d4c49fd90ae6601713a9b
tosnufc <tosnufc@gmail.com>
2026-03-19 14:43:35 +0700

    kill dialog boxes when restart the process

--- restart_scheduler.bat
+++ restart_scheduler.bat
@@ -1,11 +1,7 @@
 @echo off
-echo === Restarting scheduler on local PC ===
-.venv\Scripts\python.exe scheduler_restart.py
-echo.
-
 for /L %%i in (60,1,64) do (
     echo === Restart scheduler on 192.168.1.%%i ===
-    "C:\Program Files\Git\usr\bin\ssh.exe" user@192.168.1.%%i "d: && cd dev\clicker-game && .venv\Scripts\python.exe scheduler_restart.py && exit"
+    "C:\Program Files\Git\usr\bin\ssh.exe" user@192.168.1.%%i "d: && cd dev\clicker-game && .venv\Scripts\python.exe close_game_dialog.py && .venv\Scripts\python.exe scheduler_restart.py && exit"
     echo.
 )
 pause
